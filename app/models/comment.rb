4aa757605e6f109a5db4e78ff00a2acf766482c2
myonaingwinn <myonaingwin.gic@gmail.com>
2022-04-21 15:56:37 +0630

    visible control of article and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,16 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 
   validates :commenter, presence: true
   validates :body, presence: true
+
+  VALID_STATUSES = ["public", "private", "archived"]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == "archived"
+  end
 end
